a1f93dae25701924a7f70e3601d75efff67f19ae
xDepcio <olek.drwal@gmail.com>
2024-05-04 15:24:52 +0200

    added rejecting previous promises in makeSyncedPromise with NewPromiseRegisteredError error

--- src/lib/promise-lifo.ts
+++ src/lib/promise-lifo.ts
@@ -1,11 +1,17 @@
+export class NewPromiseRegisteredError extends Error {
+    constructor(message: string) {
+        super(message)
+    }
+}
+
 /**
  * Create function to spawn synced promises.
- * When new promise is created using this spawn function, previous one is rejected.
+ * When new promise is created using this spawn function, previous one is rejected with NewPromiseRegisteredError error.
  */
 export function makeSyncedPromise() {
     let cancel: (...args: any) => any = () => { }
     return function (promise: Promise<any>) {
-        cancel()
+        cancel(new NewPromiseRegisteredError("Promise canceled, since new promise was registred."))
         let p = new Promise((resolve, reject) => cancel = reject)
         return Promise.race([p, promise])
     }
