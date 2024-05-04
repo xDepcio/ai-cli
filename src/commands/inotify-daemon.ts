c5e8b4a5ddfb0bcb33025397eb750a5b4d7b840a
xDepcio <olek.drwal@gmail.com>
2024-05-04 15:59:51 +0200

    inotify daemon now uses makeSyncedPromise

--- src/commands/inotify-daemon.ts
+++ src/commands/inotify-daemon.ts
@@ -5,45 +5,60 @@ import { STORE } from '../index.js'
 import { curNLeft, curNRight, eraseFromCursorToEndLine } from '../lib/ansi-escapes.js'
 import { CompletionReturnData } from '../lib/copilot-api.js'
 import { CompleteBackend } from './complete.js'
+import { NewPromiseRegisteredError, makeSyncedPromise } from '../lib/promise-lifo.js'
 
-let cancel: (...args: any) => any = () => { }
 const completeBackend = new CompleteBackend()
 export default class InotifyDaemon extends Command {
+    private syncedPromise = makeSyncedPromise()
 
     private async handleReadlineAccess() {
         const [readlineLine, readlineCursor, pwd, language, prePrompt] = fs.readFileSync('/home/olek/.ai-cli/readline_access.txt', 'utf8').split('<=%SEP%=>')
         if (!readlineLine || !readlineCursor || !language || !prePrompt || !pwd) {
             return
         }
-        cancel()
-        let p = new Promise(resolve => cancel = resolve)
         let parsedReadlineCursor = parseInt(readlineCursor)
         // process.stdout.write(curNRight((readlineLine.length - 1) - parsedReadlineCursor) + eraseFromCursorToEndLine)
         // process.stdout.write(chalk.dim('     ⟳') + curNLeft(6))
         // process.stdout.write(curNLeft((readlineLine.length - 1) - parsedReadlineCursor))
         const prompt = '\n$ ' + readlineLine
-        Promise.race([p, completeBackend.getCompletions({ language, prompt, prePrompt })]).then((result) => {
-            if (result) {
-                const completions = result as CompletionReturnData[]
-                const completionsStr = completions.map(c => c.choices[0].text).join('')
-                STORE.writeTextFile('completions.txt', completionsStr)
-                process.stdout.write(curNRight((readlineLine.length - 1) - parsedReadlineCursor) + eraseFromCursorToEndLine)
-                process.stdout.write(curNLeft((readlineLine.length - 1) - parsedReadlineCursor))
-                if (completionsStr.length > 0) {
-
-                    process.stdout.write(curNRight((readlineLine.length - 1) - parsedReadlineCursor))
-                    process.stdout.write(chalk.dim(completionsStr) + curNLeft(completionsStr.length))
+        this.syncedPromise(completeBackend.getCompletions({ language, prompt, prePrompt }))
+            .then((result) => {
+                if (result) {
+                    const completions = result as CompletionReturnData[]
+                    const completionsStr = completions.map(c => c.choices[0].text).join('')
+                    STORE.writeTextFile('completions.txt', completionsStr)
+                    process.stdout.write(curNRight((readlineLine.length - 1) - parsedReadlineCursor) + eraseFromCursorToEndLine)
                     process.stdout.write(curNLeft((readlineLine.length - 1) - parsedReadlineCursor))
+                    if (completionsStr.length > 0) {
+
+                        process.stdout.write(curNRight((readlineLine.length - 1) - parsedReadlineCursor))
+                        process.stdout.write(chalk.dim(completionsStr) + curNLeft(completionsStr.length))
+                        process.stdout.write(curNLeft((readlineLine.length - 1) - parsedReadlineCursor))
+                    }
                 }
-            }
-        })
+            })
+            .catch((e) => {
+                if (e instanceof NewPromiseRegisteredError) {
+                    return
+                }
+
+                if (e instanceof TypeError) {
+                    if ((e.cause as any).code === 'UND_ERR_CONNECT_TIMEOUT') {
+                        process.stdout.write(chalk.bgRed('Copilot timeout out'))
+                    }
+                    return
+                }
+
+                console.error("not handled error", e)
+            })
     }
 
     public async run(): Promise<void> {
         const { args, flags } = await this.parse(InotifyDaemon)
+        this.syncedPromise = makeSyncedPromise()
 
         return new Promise((resolve, reject) => {
-            const watcher = fs.watch('/home/olek/.ai-cli/readline_access.txt', this.handleReadlineAccess)
+            const watcher = fs.watch('/home/olek/.ai-cli/readline_access.txt', () => this.handleReadlineAccess())
             watcher.on('close', () => {
                 resolve()
             })
