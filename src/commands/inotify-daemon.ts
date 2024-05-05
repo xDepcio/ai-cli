daa25c87df96928cdc2ffac4052c5e1d45ee275d
xDepcio <olek.drwal@gmail.com>
2024-05-06 01:09:20 +0200

    modified InotifyDameon to use new StdoutWriter

--- src/commands/inotify-daemon.ts
+++ src/commands/inotify-daemon.ts
@@ -2,26 +2,25 @@ import { Command } from '@oclif/core'
 import chalk from 'chalk'
 import fs from 'fs'
 import { STORE } from '../index.js'
-import { curNLeft, curNRight, eraseFromCursorToEndLine } from '../lib/ansi-escapes.js'
 import { CompletionReturnData } from '../lib/copilot-api.js'
-import { CompleteBackend } from './complete.js'
 import { NewPromiseRegisteredError, makeSyncedPromise } from '../lib/promise-lifo.js'
+import { StdoutWriter } from '../lib/stdout-writer.js'
+import { CompleteBackend } from './complete.js'
 
 export default class InotifyDaemon extends Command {
     private syncedPromise = makeSyncedPromise()
     private completeBackend = new CompleteBackend()
+    private writer = new StdoutWriter({ loadingMessage: ' (...)' })
 
     private async handleReadlineAccess() {
         const [readlineLine, readlineCursor, pwd, language, prePrompt] = fs.readFileSync('/home/olek/.ai-cli/readline_access.txt', 'utf8').split('<=%SEP%=>')
         if (!readlineLine || !readlineCursor || !language || !prePrompt || !pwd) {
             return
         }
+
         let parsedReadlineCursor = parseInt(readlineCursor)
-        // process.stdout.write(curNRight((readlineLine.length - 1) - parsedReadlineCursor) + eraseFromCursorToEndLine)
-        // process.stdout.write(chalk.dim('     ⟳') + curNLeft(6))
-        // process.stdout.write(curNLeft((readlineLine.length - 1) - parsedReadlineCursor))
-        const prompt = '\n$ ' + readlineLine
-        this.syncedPromise(this.completeBackend.getCompletions({ language, prompt, prePrompt }))
+        this.writer.writeLoading(readlineLine, parsedReadlineCursor)
+        this.syncedPromise(this.completeBackend.getCompletions({ language, prompt: '\n$ ' + readlineLine, prePrompt }))
             .then((result) => {
                 if (!result) {
                     return
@@ -30,15 +29,13 @@ export default class InotifyDaemon extends Command {
                 const completions = result as CompletionReturnData[]
                 const completionsStr = completions.map(c => c.choices[0].text).join('')
                 STORE.writeTextFile('completions.txt', completionsStr)
-                process.stdout.write(curNRight((readlineLine.length - 1) - parsedReadlineCursor) + eraseFromCursorToEndLine)
-                process.stdout.write(curNLeft((readlineLine.length - 1) - parsedReadlineCursor))
+
+                this.writer.clearAppended(readlineLine, parsedReadlineCursor)
                 if (completionsStr.length <= 0) {
                     return
                 }
 
-                process.stdout.write(curNRight((readlineLine.length - 1) - parsedReadlineCursor))
-                process.stdout.write(chalk.dim(completionsStr) + curNLeft(completionsStr.length))
-                process.stdout.write(curNLeft((readlineLine.length - 1) - parsedReadlineCursor))
+                this.writer.writeCompletion(readlineLine, parsedReadlineCursor, completionsStr)
             })
             .catch((e) => {
                 if (e instanceof NewPromiseRegisteredError) {
