77b92a1812ef0246222c6af4718bac43e0f1dd4f
xDepcio <olek.drwal@gmail.com>
2024-05-06 17:47:48 +0200

    removed unnecessary comments

--- src/bash/inotify-start.sh
+++ src/bash/inotify-start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# kill $(ps -ef | rg "inotifywait -q -q" | sd "olek[ ]+([0-9]+)[ ]+([0-9]+).*" '$2')
-# Define the file to monitor
 file_to_monitor="/home/olek/my-projects-2/ai-cli/strace.log"
 
 if [ -z "$1" ]; then
@@ -9,12 +7,9 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# strace -qqq -f -p $1 -s 1000 -e trace=write 2>&1 | rg --line-buffered -o 'write\(2, "[!-~ ]", 1\)' >| ./strace.log &
-# Run indefinitely, monitoring for changes to the file
 strace_size=0
 new_strace_size=0
 while true; do
-    # Use inotifywait to monitor for changes to the file
     new_strace_size=$(ls -l $file_to_monitor | awk '{print $5}')
     if [ "$strace_size" = "$new_strace_size" ]; then
         inotifywait -q -q -e modify "$file_to_monitor"
