dcc675e919bb3d839efee2762fb866da7c74c5bc
xDepcio <olek.drwal@gmail.com>
2024-05-06 02:54:11 +0200

    added shell function to fill completion

--- src/bash/inotify-source.sh
+++ src/bash/inotify-source.sh
@@ -5,3 +5,15 @@ _mt_test_f()
 }
 
 bind -x '" ": _mt_test_f'
+
+_fill_complete()
+{
+    completion=$(cat ~/.ai-cli/completions.txt)
+    completion_length=$(echo -n "$completion" | wc -c)
+
+    READLINE_LINE="$READLINE_LINE$completion"
+    READLINE_POINT=$(($READLINE_POINT + $completion_length))
+    echo -n '' >| ~/.ai-cli/completions.txt
+}
+
+bind -x '"\C- ": _fill_complete'
