5b115064e28063452fba06c7475d103a499f026b
xDepcio <olek.drwal@gmail.com>
2024-05-08 23:24:13 +0200

    added activation message in run script

--- src/bash/run.sh
+++ src/bash/run.sh
@@ -6,3 +6,6 @@ source /home/olek/my-projects-2/ai-cli/src/bash/inotify-source.sh
 /home/olek/my-projects-2/ai-cli/src/bash/strace.sh $$ &
 /home/olek/my-projects-2/ai-cli/src/bash/inotify-start.sh $$ &
 /home/olek/my-projects-2/ai-cli/bin/run.js inotify-daemon &
+
+activate_message="Terminal copilot server started in background. Press 'Alt + q' to toggle it on/off."
+echo -e "\033[34;1m${activate_message}\033[0m"
