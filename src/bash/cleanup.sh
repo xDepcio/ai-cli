cf900b51772f365a2187307a33f557e9c0760601
xDepcio <olek.drwal@gmail.com>
2024-05-10 00:50:24 +0200

    added reseting work file on cleanup

--- src/bash/cleanup.sh
+++ src/bash/cleanup.sh
@@ -10,3 +10,8 @@ inotifywait_regex='inotifywait'
 combined_regex="($node_daemon_regex)|($my_inotify_regex)|($strace_regex)|($strace_shell_regex)|($inotify_start_shell_regex)|($inotifywait_regex)"
 
 kill $(ps aux | rg "$combined_regex" | awk '{print $2}')
+
+echo -n '' >| ~/.ai-cli/completions.txt
+echo -n '' >| ~/.ai-cli/readline_access.txt
+echo -n '' >| ~/.ai-cli/readline_contents.txt
+echo -n 'on' >| ~/.ai-cli/status.txt
