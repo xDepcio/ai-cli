cfb43cf8a2cc25bcb3b6c2682d45b751e193c84b
xDepcio <olek.drwal@gmail.com>
2024-05-10 00:27:06 +0200

    DRYied logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -20,24 +20,26 @@ class Logger {
     }
 
     public info(message: string, data?: POJOStringifiableToJson) {
-        const logMessage = `${(new Date()).toISOString()} [INFO] ${message}${data ? JSON.stringify(data) : ''}`;
-        this.finishLog(logMessage);
+        this.message(message, 'INFO', data);
     }
 
     public debug(message: string, data?: POJOStringifiableToJson) {
-        const logMessage = `${(new Date()).toISOString()} [DEBUG] ${message}${data ? JSON.stringify(data) : ''}`;
-        this.finishLog(logMessage);
+        this.message(message, 'DEBUG', data);
     }
 
     public error(message: string, data?: POJOStringifiableToJson) {
-        const logMessage = `${(new Date()).toISOString()} [ERROR] ${message}${data ? JSON.stringify(data) : ''}`;
+        this.message(message, 'ERROR', data);
+    }
+
+    private message(message: string, type: string, data?: POJOStringifiableToJson) {
+        const logMessage = `${(new Date()).toISOString()} [${type}] ${message}${data ? JSON.stringify(data, null, 4) : ''}\n`;
         this.finishLog(logMessage);
     }
 
     private internal(message: string, data?: POJOStringifiableToJson) {
-        const logMessage = `${(new Date()).toISOString()} [INTERNAL] ${message}${data ? JSON.stringify(data) : ''}`;
-        this.finishLog(logMessage);
+        this.message(message, 'INTERNAL', data);
     }
+
 
     private finishLog(message: string) {
         switch (this.options.logDest) {
@@ -45,7 +47,7 @@ class Logger {
                 console.log(message);
                 break;
             case 'file':
-                fs.appendFileSync(this.options.logFile, message + '\n');
+                fs.appendFileSync(this.options.logFile, message);
                 break;
         }
     }
