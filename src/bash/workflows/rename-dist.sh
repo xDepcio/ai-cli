73d7467e20e8e5a40a915b98c860f572a5ae0401
xDepcio <olek.drwal@gmail.com>
2024-05-13 03:57:08 +0200

    replaced sd with sed

--- src/bash/workflows/rename-dist.sh
+++ src/bash/workflows/rename-dist.sh
@@ -2,7 +2,7 @@
 
 cd dist
 OLD_FILES=$(find ./ -name "*.tar.gz")
-NEW_FILES=$(find ./ -name "*.tar.gz" | sd '\-[0-9]{3,}' '')
+NEW_FILES=$(find ./ -name "*.tar.gz" | sed -E 's/-[0-9]{3,}//g')
 for i in $(seq 1 $(echo $OLD_FILES | wc -w)); do
     # mv $(echo $OLD_FILES | cut -d ' ' -f $i) $(echo $NEW_FILES | cut -d ' ' -f $i)
     mv $(echo $OLD_FILES | cut -d ' ' -f $i) $(echo $NEW_FILES | cut -d ' ' -f $i)
