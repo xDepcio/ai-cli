524d73f86acdec01da69cd669f450c56c350cdc0
xDepcio <olek.drwal@gmail.com>
2024-05-13 04:06:34 +0200

    fixed sed

--- src/bash/workflows/rename-dist.sh
+++ src/bash/workflows/rename-dist.sh
@@ -2,7 +2,7 @@
 
 cd dist
 OLD_FILES=$(find ./ -name "*.tar.gz")
-NEW_FILES=$(find ./ -name "*.tar.gz" | sed -E 's/-[0-9]{3,}//g')
+NEW_FILES=$(find ./ -name "*.tar.gz" | sed -E 's/(v[0-9]+.[0-9]+.[0-9]+)-[0-9a-zA-Z]*-/\1-/')
 for i in $(seq 1 $(echo $OLD_FILES | wc -w)); do
     # mv $(echo $OLD_FILES | cut -d ' ' -f $i) $(echo $NEW_FILES | cut -d ' ' -f $i)
     mv $(echo $OLD_FILES | cut -d ' ' -f $i) $(echo $NEW_FILES | cut -d ' ' -f $i)
