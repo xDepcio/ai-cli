86d54647b2d96cffd5ef247c7605f73fa5a4527e
xDepcio <olek.drwal@gmail.com>
2024-05-24 01:26:31 +0200

    removed junk files

--- d.sh
+++
@@ -1,3 +0,0 @@
-echo "alias ai-cli=\"\$AI_CLI_SCRIPTS_DIR/cleanup.sh
-strace -qqq -f -p \\\$$ -s 1000 -e trace=write 2>&1 | grep --line-buffered --only-matching 'write(2, \\\"[!-~ A-Za-z\*]\\\", 1)' >| \$AI_CLI_DIR/strace.log &
-source \$AI_CLI_DIR/scripts/run.sh\""
