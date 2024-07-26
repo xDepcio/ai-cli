4a82794c4f88b47c0765e56c9e34bb656cce8fa1
xDepcio <olek.drwal@gmail.com>
2024-07-26 15:55:36 +0200

    added check to passive completion daemon to automatically start daemon when it is not started

--- src/bash/install-local.sh
+++ src/bash/install-local.sh
@@ -12,6 +12,11 @@ mkdir $AI_CLI_SCRIPTS_DIR
 cp ~/my-projects/ai-cli/src/bash/* $AI_CLI_SCRIPTS_DIR -r
 chmod +x $AI_CLI_SCRIPTS_DIR/*.sh
 
-rm -rf $AI_CLI_DIR/ai-cli
-mkdir $AI_CLI_DIR/ai-cli/bin -p
-ln -s /home/olek/my-projects-2/ai-cli/bin/run.js $AI_CLI_DIR/ai-cli/bin/ai-cli
+/home/adrwal/my-projects/ai-cli/node_modules/.bin/oclif pack tarballs --parallel --no-xz
+cp ~/my-projects/ai-cli/dist/ai-cli-v0.0.1-*-linux-x64.tar.gz ~/
+cd ~/
+tar -xvf ai-cli-v0.0.1-*-linux-x64.tar.gz -C $AI_CLI_DIR
+
+# rm -rf $AI_CLI_DIR/ai-cli
+# mkdir $AI_CLI_DIR/ai-cli/bin -p
+# ln -s /home/olek/my-projects-2/ai-cli/bin/run.js $AI_CLI_DIR/ai-cli/bin/ai-cli
--- src/bash/modes/passive-node-heavy-bind-completion/source.sh
+++ src/bash/modes/passive-node-heavy-bind-completion/source.sh
@@ -2,6 +2,16 @@
 
 _complete_terminal()
 {
+    if [ -z "$(pgrep -f "./bin/ai-cli passive-completion-daemon")" ]; then
+        starting_message="Starting passive completion daemon..."
+        echo -e "\033[34;1m${starting_message}\033[0m"
+
+        $AI_CLI_DIR/ai-cli/bin/ai-cli passive-completion-daemon &
+
+        success_message="Daemon started successfully."
+        echo -e "\033[32;1m${success_message}\033[0m"
+    fi
+
     prompt="$ ls -al
 $(ls -al $PWD)
 $ $READLINE_LINE"
