ea87cffa0d88b954c2bb9ccb452375f98ef750e9
xDepcio <olek.drwal@gmail.com>
2024-07-27 10:17:20 +0200

    added bash completions to passive mode source

--- src/bash/modes/passive-node-heavy-bind-completion/source.sh
+++ src/bash/modes/passive-node-heavy-bind-completion/source.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+_fill_complete()
+{
+    completion=$(cat $AI_CLI_DIR/passive-completion.txt)
+    completion_length=$(echo -n "$completion" | wc -c)
+
+    READLINE_LINE="$READLINE_LINE$completion"
+    READLINE_POINT=$(($READLINE_POINT + $completion_length))
+    echo -n '' >| $AI_CLI_DIR/passive-completion.txt
+}
+
+bind -x '"\C- ": _fill_complete'
+
+_fill_word()
+{
+    next_word=$(rg '^ *[!-~]+' $AI_CLI_DIR/passive-completion.txt -o)
+    next_word_length=$(echo -n "$next_word" | wc -c)
+    sd '^ *[!-~]+' '' $AI_CLI_DIR/passive-completion.txt
+    rest=$(cat $AI_CLI_DIR/passive-completion.txt)
+    rest_length=$(echo -n "$rest" | wc -c)
+
+    READLINE_LINE="$READLINE_LINE$next_word"
+    READLINE_POINT=$(($READLINE_POINT + $next_word_length))
+    if [ $rest_length = 0 ]; then
+        return
+    fi
+
+    ( sleep 0 && echo -e -n "\033[2m$rest\033[0m\033[${rest_length}D" & )
+}
+bind -x '"\C-n": _fill_word'
+
 _complete_terminal()
 {
     if [ -z "$(pgrep -f "./bin/ai-cli passive-completion-daemon")" ]; then
