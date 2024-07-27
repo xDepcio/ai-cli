ea13ffb754ebc0712d00facc0be4619706240d2b
xDepcio <olek.drwal@gmail.com>
2024-07-27 10:17:42 +0200

    added creation of files to passive mode daemon

--- src/commands/passive-completion-daemon.ts
+++ src/commands/passive-completion-daemon.ts
@@ -3,26 +3,32 @@ import chalk from 'chalk'
 import fs from 'fs'
 import { curNLeft, curNRight } from '../lib/ansi-escapes.js'
 import { checkProcessExists } from '../lib/process-checker.js'
+import path from 'path'
+import { STORE_DIR_PATH } from '../constants.js'
 
 
 export default class PassiveCompletionDaemon extends Command {
     private timeout: NodeJS.Timeout = setTimeout(() => { }, 0)
+    private readonly completionFile = path.join(STORE_DIR_PATH, 'passive-completion.txt')
+    private readonly readlineLineFile = path.join(STORE_DIR_PATH, 'readline-line.txt')
+    private readonly readlinePointFile = path.join(STORE_DIR_PATH, 'readline-point.txt')
+    private readonly passiveCompletionTriggerPidFile = path.join(STORE_DIR_PATH, 'passive-completion-trigger-pid.txt')
 
     private readCompletion() {
-        const completion = fs.readFileSync('/home/adrwal/passive-completion.txt', 'utf8')
+        const completion = fs.readFileSync(this.completionFile, 'utf8')
         return completion
     }
 
     private readReadlineData() {
-        const readlineLine = fs.readFileSync('/home/adrwal/readline-line.txt', 'utf8')
-        const readlinePoint = fs.readFileSync('/home/adrwal/readline-point.txt', 'utf8')
+        const readlineLine = fs.readFileSync(this.readlineLineFile, 'utf8')
+        const readlinePoint = fs.readFileSync(this.readlinePointFile, 'utf8')
         return { readlineLine, readlinePoint }
     }
 
     private getPassiveCompletionTriggerPid() {
         while (true) {
             try {
-                const pid = fs.readFileSync('/home/adrwal/passive-completion-trigger-pid.txt', 'utf8')
+                const pid = fs.readFileSync(this.passiveCompletionTriggerPidFile, 'utf8')
                 return parseInt(pid)
             }
             catch (e) { }
@@ -34,11 +40,28 @@ export default class PassiveCompletionDaemon extends Command {
         return this.timeout._destroyed
     }
 
+    private makeRequiredFiles() {
+        if (!fs.existsSync(this.completionFile)) {
+            fs.writeFileSync(this.completionFile, '', 'utf8')
+        }
+        if (!fs.existsSync(this.readlineLineFile)) {
+            fs.writeFileSync(this.readlineLineFile, '', 'utf8')
+        }
+        if (!fs.existsSync(this.readlinePointFile)) {
+            fs.writeFileSync(this.readlinePointFile, '', 'utf8')
+        }
+        if (!fs.existsSync(this.passiveCompletionTriggerPidFile)) {
+            fs.writeFileSync(this.passiveCompletionTriggerPidFile, '', 'utf8')
+        }
+    }
+
     public async run(): Promise<void> {
         // const { args, flags } = await this.parse(PassiveCompletionDaemon)
 
+        this.makeRequiredFiles()
+
         return new Promise((resolve, reject) => {
-            fs.watch('/home/adrwal/passive-completion.txt', {}, async (eventType, filename) => {
+            fs.watch(this.completionFile, {}, async (eventType, filename) => {
                 const triggerPid = this.getPassiveCompletionTriggerPid()
                 while (checkProcessExists(triggerPid)) { }
 
