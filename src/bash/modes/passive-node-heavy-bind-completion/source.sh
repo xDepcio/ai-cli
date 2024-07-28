9f4a5413893d41b362d3bebcd7bc58aa37e12f00
xDepcio <olek.drwal@gmail.com>
2024-07-28 13:49:30 +0200

    changed communication from file based to signal based. Could remove big portion of synchronization fixes due to this change

--- src/bash/modes/passive-node-heavy-bind-completion/source.sh
+++ src/bash/modes/passive-node-heavy-bind-completion/source.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+AI_CLI_PASSIVE_COMPLETION_DAEMON_PID_FILE="$AI_CLI_DIR/passive-completion-daemon-pid.txt"
+
 _fill_complete()
 {
-    completion=$(cat $AI_CLI_DIR/passive-completion.txt)
-    completion_length=$(echo -n "$completion" | wc -c)
+    # echo $READLINE_LINE
+    # echo $READLINE_POINT
+    # completion=$(cat $AI_CLI_DIR/passive-completion.txt)
+    # completion_length=${#completion}
 
-    READLINE_LINE="$READLINE_LINE$completion"
-    READLINE_POINT=$(($READLINE_POINT + $completion_length))
-    echo -n '' >| $AI_CLI_DIR/passive-completion.txt
+    # READLINE_LINE="$READLINE_LINE$completion"
+    # READLINE_POINT=1
+
+    # echo $READLINE_LINE
+    # echo $READLINE_POINT
+
+    # echo -n '' >| $AI_CLI_DIR/passive-completion.txt
+    echo xd > /dev/null
 }
 
 bind -x '"\C- ": _fill_complete'
@@ -32,12 +41,14 @@ bind -x '"\C-n": _fill_word'
 
 _complete_terminal()
 {
-    if [ -z "$(pgrep -f "./bin/ai-cli passive-completion-daemon")" ]; then
+    if [ -z "$(pgrep -f "ai-cli/bin/run.js passive-completion-daemon")" ]; then
         starting_message="Starting passive completion daemon..."
         echo -e "\033[34;1m${starting_message}\033[0m"
 
-        $AI_CLI_DIR/ai-cli/bin/ai-cli passive-completion-daemon &
+        $AI_CLI_DIR/ai-cli/bin/node $AI_CLI_DIR/ai-cli/bin/run.js passive-completion-daemon &
+        echo $! >| $AI_CLI_PASSIVE_COMPLETION_DAEMON_PID_FILE
 
+        sleep 1 # make sure signal handler mounts
         success_message="Daemon started successfully."
         echo -e "\033[32;1m${success_message}\033[0m"
     fi
@@ -47,11 +58,15 @@ $(ls -al $PWD)
 $ $READLINE_LINE"
 
     completion=$($AI_CLI_DIR/ai-cli/bin/ai-cli complete -l bash -t "$prompt")
+    # completion="xddd"
+    completion_daemon_pid=$(cat $AI_CLI_PASSIVE_COMPLETION_DAEMON_PID_FILE)
 
     echo -n "$READLINE_LINE" >| $AI_CLI_DIR/readline-line.txt
     echo -n "$READLINE_POINT" >| $AI_CLI_DIR/readline-point.txt
-    read < <( echo -n "$completion" >| $AI_CLI_DIR/passive-completion.txt & echo $! ) # this write triggers daemon
-    echo -n $REPLY >| $AI_CLI_DIR/passive-completion-trigger-pid.txt
+    echo -n "$completion" >| $AI_CLI_DIR/passive-completion.txt
+    kill -SIGUSR2 $completion_daemon_pid
+    # read < <( echo -n "$completion" >| $AI_CLI_DIR/passive-completion.txt & echo $! ) # this write triggers daemon
+    # echo -n $REPLY >| $AI_CLI_DIR/passive-completion-trigger-pid.txt
 }
 
 bind -x '"\eq": _complete_terminal'
--- src/commands/passive-completion-daemon.ts
+++ src/commands/passive-completion-daemon.ts
@@ -12,7 +12,7 @@ export default class PassiveCompletionDaemon extends Command {
     private readonly completionFile = path.join(STORE_DIR_PATH, 'passive-completion.txt')
     private readonly readlineLineFile = path.join(STORE_DIR_PATH, 'readline-line.txt')
     private readonly readlinePointFile = path.join(STORE_DIR_PATH, 'readline-point.txt')
-    private readonly passiveCompletionTriggerPidFile = path.join(STORE_DIR_PATH, 'passive-completion-trigger-pid.txt')
+    private readonly keepAliveFile = path.join(STORE_DIR_PATH, 'keep-alive.txt')
 
     private readCompletion() {
         const completion = fs.readFileSync(this.completionFile, 'utf8')
@@ -25,21 +25,6 @@ export default class PassiveCompletionDaemon extends Command {
         return { readlineLine, readlinePoint }
     }
 
-    private getPassiveCompletionTriggerPid() {
-        while (true) {
-            try {
-                const pid = fs.readFileSync(this.passiveCompletionTriggerPidFile, 'utf8')
-                return parseInt(pid)
-            }
-            catch (e) { }
-        } // >:(
-    }
-
-    private canComplete() {
-        // @ts-ignore
-        return this.timeout._destroyed
-    }
-
     private makeRequiredFiles() {
         if (!fs.existsSync(this.completionFile)) {
             fs.writeFileSync(this.completionFile, '', 'utf8')
@@ -50,9 +35,28 @@ export default class PassiveCompletionDaemon extends Command {
         if (!fs.existsSync(this.readlinePointFile)) {
             fs.writeFileSync(this.readlinePointFile, '', 'utf8')
         }
-        if (!fs.existsSync(this.passiveCompletionTriggerPidFile)) {
-            fs.writeFileSync(this.passiveCompletionTriggerPidFile, '', 'utf8')
+    }
+
+    private handleCompletionSugestionWrite() {
+        const completion = this.readCompletion()
+        const { readlineLine, readlinePoint } = this.readReadlineData()
+
+        const cursorDistFromEnd = readlineLine.length - parseInt(readlinePoint)
+        if (cursorDistFromEnd > 0) {
+            process.stdout.write(curNRight(cursorDistFromEnd))
         }
+        process.stdout.write(chalk.dim(completion))
+        process.stdout.write(curNLeft(completion.length))
+        if (cursorDistFromEnd > 0) {
+            process.stdout.write(curNLeft(cursorDistFromEnd))
+        }
+    }
+
+    private keepAlive() {
+        if (!fs.existsSync(this.keepAliveFile)) {
+            fs.writeFileSync(this.keepAliveFile, '', 'utf8')
+        }
+        fs.watch(this.keepAliveFile, {}, async (eventType, filename) => { })
     }
 
     public async run(): Promise<void> {
@@ -60,29 +64,13 @@ export default class PassiveCompletionDaemon extends Command {
 
         this.makeRequiredFiles()
 
+
         return new Promise((resolve, reject) => {
-            fs.watch(this.completionFile, {}, async (eventType, filename) => {
-                const triggerPid = this.getPassiveCompletionTriggerPid()
-                while (checkProcessExists(triggerPid)) { }
-
-                if (!this.canComplete()) {
-                    return
-                }
-                this.timeout = setTimeout(() => { }, 500)
-
-                const completion = this.readCompletion()
-                const { readlineLine, readlinePoint } = this.readReadlineData()
-
-                const cursorDistFromEnd = readlineLine.length - parseInt(readlinePoint)
-                if (cursorDistFromEnd > 0) {
-                    process.stdout.write(curNRight(cursorDistFromEnd))
-                }
-                process.stdout.write(chalk.dim(completion))
-                process.stdout.write(curNLeft(completion.length))
-                if (cursorDistFromEnd > 0) {
-                    process.stdout.write(curNLeft(cursorDistFromEnd))
-                }
+            process.on('SIGUSR2', () => {
+                this.handleCompletionSugestionWrite()
             })
+
+            this.keepAlive()
         })
     }
 }
