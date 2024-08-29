7e97886b6161992460a580eac23bbd33e46b392c
xDepcio <olek.drwal@gmail.com>
2024-08-29 23:39:37 +0200

    remove chat test skeleton

--- test/commands/chat.test.ts
+++ test/commands/chat.test.ts
@@ -1,14 +1,14 @@
-import {runCommand} from '@oclif/test'
-import {expect} from 'chai'
+// import {runCommand} from '@oclif/test'
+// import {expect} from 'chai'
 
-describe('chat', () => {
-  it('runs chat cmd', async () => {
-    const {stdout} = await runCommand('chat')
-    expect(stdout).to.contain('hello world')
-  })
+// describe('chat', () => {
+//   it('runs chat cmd', async () => {
+//     const {stdout} = await runCommand('chat')
+//     expect(stdout).to.contain('hello world')
+//   })
 
-  it('runs chat --name oclif', async () => {
-    const {stdout} = await runCommand('chat --name oclif')
-    expect(stdout).to.contain('hello oclif')
-  })
-})
+//   it('runs chat --name oclif', async () => {
+//     const {stdout} = await runCommand('chat --name oclif')
+//     expect(stdout).to.contain('hello oclif')
+//   })
+// })
