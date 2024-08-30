7e62899a5c58df2fba066234ed1a7e1467b6092b
xDepcio <olek.drwal@gmail.com>
2024-08-30 12:53:09 +0200

    spacing fix for chat.ts command

--- src/commands/chat.ts
+++ src/commands/chat.ts
@@ -1,30 +1,30 @@
-import {Args, Command, Flags} from '@oclif/core'
+import { Args, Command, Flags } from '@oclif/core'
 
 export default class Chat extends Command {
-  static override args = {
-    file: Args.string({description: 'file to read'}),
-  }
-
-  static override description = 'describe the command here'
-
-  static override examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
-
-  static override flags = {
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
-  }
-
-  public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Chat)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /home/olek/my-projects-2/ai-cli/src/commands/chat.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    static override args = {
+        file: Args.string({ description: 'file to read' }),
     }
-  }
+
+    static override description = 'describe the command here'
+
+    static override examples = [
+        '<%= config.bin %> <%= command.id %>',
+    ]
+
+    static override flags = {
+        // flag with no value (-f, --force)
+        force: Flags.boolean({ char: 'f' }),
+        // flag with a value (-n, --name=VALUE)
+        name: Flags.string({ char: 'n', description: 'name to print' }),
+    }
+
+    public async run(): Promise<void> {
+        const { args, flags } = await this.parse(Chat)
+
+        const name = flags.name ?? 'world'
+        this.log(`hello ${name} from /home/olek/my-projects-2/ai-cli/src/commands/chat.ts`)
+        if (args.file && flags.force) {
+            this.log(`you input --force and --file: ${args.file}`)
+        }
+    }
 }
